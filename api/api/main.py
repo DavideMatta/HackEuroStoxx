be4846f9d2a1610fa47f9cdd8284dcbcbe9f593d
DavideMatta <133046537+DavideMatta@users.noreply.github.com>
2024-05-19 16:23:09 +0200

    Update main.py

--- api/api/main.py
+++ api/api/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from processing.pipeline import financial_data_pipeline
+#from processing.pipeline import financial_data_pipeline
 
 
 app = FastAPI(
@@ -40,13 +40,25 @@ class TradingDay(BaseModel):
     Close : float
     Adj_Close : float
     Volume : float
+    BBL_20_2: float
+    BBM_20_2: float 
+    BBU_20_2: float
+    BBB_20_2: float
+    BBP_20_2: float
+    RSI: float
+    Month_sin: float
+    Month_cos: float
+    Weekday_sin: float
+    Weekday_cos: float
+    Date_numeric: float
+    Close_t1: float
 
 @app.post("/predict/")
 def predict(trading_data: List[TradingDay]) -> float:
     X = np.array([dict(trading_day) for trading_day in trading_data])
 
-    X_preprocessed = financial_data_pipeline.fit_transform(X)
+    #X_preprocessed = financial_data_pipeline.fit_transform(X)
 
-    y_pred = model.predict(X_preprocessed)
+    y_pred = model.predict(X)
     
     return list(y_pred)
