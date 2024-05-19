c073039b5fe5e053abe30635b6815874dab35bef
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-05-19 11:43:42 +0200

    api completed

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from processing.pipeline import financial_data_pipeline
 
 
 app = FastAPI(
@@ -37,24 +38,15 @@ class TradingDay(BaseModel):
     High : float
     Low : float
     Close : float
+    Adj_Close : float
     Volume : float
-    BBL_20_2 : float
-    BBM_20_2 : float
-    BBU_20_2 : float
-    BBB_20_2 : float
-    BBP_20_2 : float
-    RSI : float
-    Month_sin : float
-    Month_cos : float
-    Weekday_sin : float
-    Weekday_cos : float
-    Date_numeric : float
-    Close_t1 : float
-
-# TODO: Change in order to have the correct structure to handle ndarrays for the GRU model
 
 @app.post("/predict/")
 def predict(trading_data: List[TradingDay]) -> float:
     X = np.array([dict(trading_day) for trading_day in trading_data])
-    y_pred = model.predict(X)
+
+    X_preprocessed = financial_data_pipeline.fit_transform(X)
+
+    y_pred = model.predict(X_preprocessed)
+    
     return list(y_pred)
